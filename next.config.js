92ef1f8dc3223bbcf6fccf570408db16ea83649d
techotaku1 <jsdg1818@gmail.com>
2024-10-25 10:58:36 -0500

    cabmios

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    // images: {
-    //   unoptimized: true, // Esto permite cargar imágenes sin optimización
-    // },
+    output: 'export',
+    images: {
+      unoptimized: true, // Esto permite cargar imágenes sin optimización
+    },
   };
   
   module.exports = nextConfig;
