980f940f01bb195ccb3c00b3d9f1229297d00064
techotaku1 <jsdg1818@gmail.com>
2024-11-11 21:22:24 -0500

    sdds

--- src/app/noticias/loading.tsx
+++ src/app/noticias/loading.tsx
@@ -1,10 +1,9 @@
 // app/noticias/loading.tsx
 export default function Loading() {
-    return (
-      <div className="loading-container">
-        <p>Cargando...</p>
-        <div className="spinner"></div> {/* Aquí podrías poner un spinner o un skeleton loader */}
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex justify-center items-center space-x-4">
+      <div className="animate-spin w-8 h-8 border-4 border-t-4 border-blue-500 rounded-full"></div>
+      <span>Cargando...</span>
+    </div>
+  );
+}
