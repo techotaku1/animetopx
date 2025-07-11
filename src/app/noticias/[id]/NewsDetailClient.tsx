0f4ed59e411dcc315b1f742ef82231e7231c9552
techotaku1 <jsdg1818@gmail.com>
2025-07-11 01:24:57 -0500

    full react

--- src/app/noticias/[id]/NewsDetailClient.tsx
+++ src/app/noticias/[id]/NewsDetailClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
